1e691ce33006be4b669249a5cc6263e4f810bb66
Harsh Tandiya <harsh.tandiya@gmail.com>
2026-01-16 19:50:21 +0530

    feat: custom success pages

--- frontend/src/components/submission/SuccessSection.vue
+++ frontend/src/components/submission/SuccessSection.vue
@@ -1,5 +1,24 @@
 <script setup lang="ts">
 import { DotLottieVue } from "@lottiefiles/dotlottie-vue";
+import { useSubmissionForm } from "@/stores/submissionForm";
+import { computed } from "vue";
+import { TextEditor } from "frappe-ui";
+
+const submissionFormStore = useSubmissionForm();
+
+const successTitle = computed(() => {
+    if (!submissionFormStore.formResource.data?.success_title) {
+        return "Form submitted successfully";
+    }
+    return submissionFormStore.formResource.data.success_title;
+});
+
+const successDescription = computed(() => {
+    if (!submissionFormStore.formResource.data?.success_description) {
+        return "Thank you for submitting the form. We will get back to you soon.";
+    }
+    return submissionFormStore.formResource.data.success_description;
+});
 </script>
 <template>
     <div class="flex flex-col gap-4 items-center">
@@ -10,7 +29,11 @@ import { DotLottieVue } from "@lottiefiles/dotlottie-vue";
             :height="150"
             class="h-[150px]"
         />
-        <h3 class="text-2xl font-bold">Form submitted successfully</h3>
-        <p class="text-sm">Thank you for submitting the form. We will get back to you soon.</p>
+        <h2 class="text-3xl font-bold">{{ successTitle }}</h2>
+        <TextEditor
+            :model-value="successDescription"
+            editor-class="h-fit !w-full form-description !px-0 max-w-full max-h-full"
+            :editable="false"
+        />
     </div>
 </template>

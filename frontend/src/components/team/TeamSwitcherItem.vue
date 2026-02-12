5058adfe81b4a562aa8ed3d25de90dbe64430e17
Harsh Tandiya <harsh.tandiya@gmail.com>
2026-02-12 18:58:30 +0530

    feat: implement TeamSwitcher component and related UI updates
    - Added TeamSwitcher and TeamSwitcherItem components for team management.
    - Integrated TeamSwitcher into BaseLayout, replacing the previous sidebar header.
    - Updated auto-imports.d.ts to include global declarations.
    - Enhanced BaseLayout by removing unused code related to team switching functionality.

---
+++ frontend/src/components/team/TeamSwitcherItem.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+type props = {
+    label: string;
+    imgUrl?: string;
+};
+
+const props = defineProps<props>();
+</script>
+<template>
+    <div class="flex items-center gap-2 shrink-0">
+        <img :src="imgUrl" alt="Team Logo" class="size-5 rounded-sm" />
+        <span class="text-base font-medium text-ink-gray-7 whitespace-nowrap">
+            {{ label }}
+        </span>
+    </div>
+</template>

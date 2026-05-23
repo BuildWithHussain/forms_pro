2c8ad6d05b32a0a765927e1a72d76bb12a7857bf
Harsh Tandiya <harsh.tandiya@gmail.com>
2026-05-23 15:52:15 +0530

    chore: refactor page loading indicator

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,19 @@
 <script setup lang="ts">
 import "vue-sonner/style.css";
 import { Toaster } from "vue-sonner";
-import { LoadingIndicator } from "frappe-ui";
 import GlobalDialog from "@/components/ui/GlobalDialog.vue";
-import { useRouteData } from "@/stores/routeData";
+import RouteProgress from "@/components/RouteProgress.vue";
 import { useUser } from "@/stores/user";
 
 const userStore = useUser();
 userStore.initialize();
-
-const routeData = useRouteData();
 </script>
 
 <template>
     <div>
         <router-view />
     </div>
-    <LoadingIndicator v-if="routeData.isNavigating" class="fixed top-4 right-4 z-50" />
+    <RouteProgress />
     <Toaster theme="system" />
     <GlobalDialog />
 </template>
